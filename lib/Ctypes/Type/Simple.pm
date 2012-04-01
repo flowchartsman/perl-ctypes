flowchartsman/perl-ctypes

commit 3edca28e35d6a56c7ac2711f34641151bcdd3ed4

libffi 3.0.10rc3 msys fixes pod formatting s/allow_overflow/strict_input/g fix Type init   force _update_ and validate data   set VALUE slot   call hook_in

lib/Ctypes/Type/Simple.pm
@@ -3,10 +3,10 @@ use strict;
 use warnings;
 use Carp;
 use Ctypes;
-use Ctypes::Type qw|&_types &allow_overflow_all|;
+use Ctypes::Type qw|&_types &strict_input_all|;
 our @ISA = qw|Ctypes::Type|;
 use fields qw|alignment name _typecode size
-              allow_overflow val _as_param_|;
+              strict_input val _as_param_|;
 use overload '${}' => \&_scalar_overload,
              '0+'  => \&_scalar_overload,
              '""'  => \&_scalar_overload,
@@ -150,10 +150,18 @@ sub new {
   } );
   #for(keys(%{$attrs})) { $self->{$_} = $attrs->{$_}; };
   #bless $self => $class;
-  $arg = 0 unless defined $arg;
+  if (defined $arg) {
+    $self->{_datasafe} = 0; # force _update_ and validate data
+    if( exists $Ctypes::Type::_types->{$typecode}->{hook_in} ) {
+      my ($valid, $newarg) = $Ctypes::Type::_types->{$typecode}->{hook_in}->($arg);
+      $arg = $newarg unless $valid;
+    }
+  } else {
+    $arg = 0;
+  }
   $self->{_value} = $arg;
   $self->{_rawvalue} = tie $self->{_value}, 'Ctypes::Type::Simple::value', $self;
-  $self->{_rawvalue}{VALUE} = $arg if not defined $self->{_rawvalue}{VALUE};
+  $self->{_rawvalue}{VALUE} = $arg;
   return $self;
 }
 
@@ -176,7 +184,7 @@ sub strict_input {
     my $self = shift;
     my $arg = shift;
     if( @_  or ( defined $arg and $arg != 1 and $arg != 0 ) ) {
-      croak("Usage: allow_overflow(1 or 0)");
+      croak("Usage: strict_input(1 or 0)");
     }
     $self->{_strict_input} = $arg if defined $arg;
     $self->{_strict_input};
@@ -235,7 +243,7 @@ sub data {
     return \$self->{_data};
   }
   $self->{_data} =
-    pack( $self->{_typecode}, $self->{_rawvalue}{VALUE} );
+    pack( $self->packcode, $self->{_rawvalue}{VALUE} );
   print "    returning ", unpack('b*',$self->{_data}), "\n" if $Debug;
   $self->{_datasafe} = 0;  # used by FETCH
   return \$self->{_data};
@@ -261,16 +269,18 @@ sub _update_ {
                                $self->{_index},
                                $self->size );
       print "    My data is now:\n", unpack('b*', $self->{_data}), "\n" if $Debug;
-      print "    Which is ", unpack($self->{_typecode},$self->{_data}), " as a number\n" if $Debug;
-  $self->{_rawvalue}{VALUE} = unpack($self->{_typecode},$self->{_data});
+      print "    Which is ", unpack($self->packcode,$self->{_data}), " as a number\n" if $Debug;
+      $self->{_rawvalue}{VALUE} = unpack($self->packcode, $self->{_data});
+    } else {
+      $self->{_data} = pack($self->packcode, $self->{_value});
     }
   } else {
-    $self->{_data} = $arg if $arg;
+    $self->{_data} = pack($self->packcode, $arg );
     if( $self->owner ) {
-      $self->owner->_update_($self->{_data},$self->{_index});
+      $self->owner->_update_($self->{_data}, $self->{_index});
     }
   }
-  $self->{_rawvalue}{VALUE} = unpack($self->{_typecode},$self->{_data});
+  $self->{_rawvalue}{VALUE} = unpack($self->packcode, $self->{_data});
   print "    VALUE is _update_d to ", $self->{_rawvalue}{VALUE}, "\n" if $Debug;
   $self->{_datasafe} = 1;
   return 1;
@@ -302,7 +312,7 @@ sub TIESCALAR {
   my $class = shift;
   my $object = shift;
   my $self = { object  => $object,
-               VALUE   => undef,
+               VALUE   => $object->{_value},
              };
   return bless $self => $class;
 }
@@ -318,6 +328,10 @@ sub STORE {
   # The following section may be removed completely to allow different
   # Types full discretion of their own input validation via hook_in().
 
+  if( exists $Ctypes::Type::_types->{$self->{object}->{_typecode}}->{hook_in} ) {
+    my ($valid, $newarg) = $Ctypes::Type::_types->{$self->{object}->{_typecode}}->{hook_in}->($arg);
+    $arg = $newarg unless $valid;
+  }
   # Deal with being assigned other Type objects and the like...
 #    if(my $ref = ref($arg)) {
 #      if($ref =~ /^Ctypes::Type::/) {
@@ -338,6 +352,7 @@ sub STORE {
   # with null (i.e. numeric zero) , update owners, return early.
   if( not defined $arg ) {
     print "    Assigned undef. All goes null.\n" if $Debug;
+    $self->{object}{_datasafe} = 0;
     $self->{VALUE} = 0;
     $self->{object}{_data} = "\0" x 8 x $self->{object}{_size}; # stay right length
     if( $self->{object}{_owner} ) {

Makefile.PL
@@ -1,21 +1,23 @@
-# perl Makefile.PL INCDIR=/usr/src/libffi-3.0.9/include LIBDIR=/usr/src/libffi-3.0.9/lib
-use 5.010000;
+# Usage:
+# perl Makefile.PL INCDIR=/usr/src/libffi-3.0.10/include LIBDIR=/usr/src/libffi-3.0.10/lib
+
+#use 5.010000;
 use ExtUtils::MakeMaker;
 use Config;
-use File::Copy 'cp';
+use File::Copy;
 use File::Spec;
 
-our $libffi_version = "3.0.10rc0";
+our $libffi_version = "3.0.10rc3";
 
 # check the installed version of libffi and override default libdir and incdir
 use lib qw(inc);
 use Devel::CheckLib;
 my ($libdir, $incdir);
 if (grep /INCDIR[ =](\S+)/,join(" ", @ARGV)) {
-  $incdir=$1;
+  $incdir = $1;
 }
 if (grep /LIBDIR[ =](\S+)/,join(" ", @ARGV)) {
-  $libdir=$1;
+  $libdir = $1;
 }
 if ($libdir or $incdir) {
   eval qq/assert_lib( lib => 'ffi', header => 'ffi.h', libpath => $libdir, incpath => $incdir )/;
@@ -24,8 +26,8 @@ if ($libdir or $incdir) {
 }
 if ($@) {
   use File::Spec;
-  warn "libffi not installed or failing. Use our own libffi $libffi_version\n";
-  if ($^O ne 'MSWin32') {
+  if ($^O !~ /MSWin32|msys/) {
+    warn "libffi not installed or failing. Use our own libffi $libffi_version\n";
     use Cwd;
     my $tar = $Config{tar} ? $Config{tar} : "tar";
     system ($tar, "xfz", "libffi.tar.gz");
@@ -38,19 +40,19 @@ if ($@) {
     $libdir = "$cwd/lib";
     if ($^O eq 'cygwin') { # copy the bin/*.dll
       mkdir "bin";
-      cp('bin/libffi-*.dll', 'bin/');
+      File::Copy::cp('bin/libffi-*.dll', 'bin/');
     }
     chdir "..";
-  } else {
+  } else { # MSWin32
     warn "Using the builtin win32 binaries $libffi_version\n";
     # built with mingw32
     # install via http://sourceforge.net/projects/mingw/files/Automated%20MinGW%20Installer/mingw-get-inst/
     my $cwd = getcwd;
     $libdir = "win32";
-    $incdir = File::Spec->catfile($cwd, "win32","include");
+    $incdir = File::Spec->catfile($cwd, $libdir,"include");
     # be sure to install the dll on site, because we link only to the import lib
     mkdir "bin";
-    cp(File::Spec->catfile('win32', "libffi-5.dll"), "bin");
+    File::Copy::cp(File::Spec->catfile($libdir, "libffi-5.dll"), "bin");
   }
 } else {
   warn "Using system libffi.\n";
@@ -99,7 +101,7 @@ if  (eval {require ExtUtils::Constant; 1}) {
 else {
   foreach my $file ('const-c.inc', 'const-xs.inc') {
     my $fallback = File::Spec->catfile('win32', $file);
-    copy ($fallback, $file) or die "Can't copy $fallback to $file: $!";
+    File::Copy::cp ($fallback, $file) or die "Can't copy $fallback to $file: $!";
   }
 }
 

README
@@ -3,24 +3,20 @@ Ctypes
 Call and wrap native C functions and libraries from Perl, using only
 Perl.
 
-This module requires the C library libffi to run. If you're on a unix-
-like system, you should install this via your favoured package
-manager. If you're on Win32 (using Strawberry Perl) you should go
-download the libffi-3.0.9 sources yourself from the following URL,
-compile the libraries and put them somewhere your Strawberry compiler
-toolchain will find them when it needs to. I can't remember just now
-whether the Strawberry toolchain is sufficient to compile libffi; you
-might be lucky.
-
-http://sourceware.org/libffi/
-
-Ctypes (current v0.001) is based on libffi to provide a way of calling
+Ctypes (current v0.003) is based on libffi to provide a way of calling
 native C library functions directly from Perl, using only Perl, and
 hence not requiring a compiler.
 
-Of course, the Ctypes module itself is written with XS. Haven't really
-thought that one through entirely. Perhaps we'll provide binaries? Or
-perhaps (wonder of wonders) it could someday be rolled into Perl5 core?
+Of course, the Ctypes module itself is written with XS, so you need 
+a compiler to install this module from source.
+Orperhaps (wonder of wonders) it could someday be rolled into Perl5 core?
+
+This module requires the C library libffi to run. If you're on a POSIX
+system, libffi should have been installed this via your favoured package
+manager, otherwise we will build and install it for you into /usr/local. 
+If you're on Win32 there are pre-compiled libffi binaries included.
+
+http://sourceware.org/libffi/
 
 INSTALLATION
 
@@ -29,7 +25,7 @@ get Module::Build to compile stuff the way I wanted. Module::Install
 is currently still a bit limited for this fancy stuff too. So what
 you do is:
 
-	perl Makefile.PL
+	perl Makefile.PL [INCDIR=libffi-include-path LIBDIR=libffi-lib-path]
 	make
 	make test
 	make install

lib/Ctypes.pm
@@ -106,14 +106,14 @@ Typecode: Ctype                  perl Packcode
   'q': c_longlong                q
   'Q': c_ulonglong               Q
 
-  's': c_char_p (ASCIIZ string) A?
-  'w': c_wchar                  U
-  'z': c_wchar_p                U*
+  's': c_char_p (ASCIIZ string)  A?
+  'w': c_wchar                   U
+  'z': c_wchar_p                 U*
 
 =cut
 
 
-=head1 SUBROUTINES
+=head1 FUNCTIONS
 
 =over
 
@@ -545,7 +545,7 @@ Call:
 
   $lib = WinDLL->kernel32;
 
-Windows only: Returns a library object for the Windows kernel32.dll.
+Windows only: Returns a library object for the Windows F<kernel32.dll>.
 
 =head1 OleDLL
 
@@ -646,6 +646,7 @@ Searches the dll/so loadpath for the given library, architecture dependently.
 The lib argument is either part of a filename (e.g. "kernel32"),
 a full pathname to the shared library
 or the same as for L<DynaLoader::dl_findfile>:
+
 "-llib" or "-Lpath -llib", with -L for the optional path.
 
 Returns the path of the found library or undef.
@@ -929,7 +930,7 @@ sub load_error() {
 
 =item addressof (obj)
 
-Returns the address of the memory buffer as integer. obj must be an
+Returns the address of the memory buffer as integer. C<obj> must be an
 instance of a ctypes type.
 
 =cut
@@ -944,7 +945,7 @@ sub addressof($) {
 =item alignment(obj_or_type)
 
 Returns the alignment requirements of a Ctypes type.
-obj_or_type must be a Ctypes type or instance.
+C<obj_or_type> must be a Ctypes type or instance.
 
 =cut
 
@@ -957,9 +958,9 @@ sub alignment($) {
 
 =item byref(obj)
 
-Returns a light-weight pointer to obj, which must be an instance of a
-ctypes type. The returned object can only be used as a foreign
-function call parameter. It behaves similar to pointer(obj), but the
+Returns a light-weight pointer to C<obj>, which must be an instance of a
+Ctypes type. The returned object can only be used as a foreign
+function call parameter. It behaves similar to C<pointer(obj)>, but the
 construction is a lot faster.
 
 =cut
@@ -968,11 +969,11 @@ sub byref {
   return \$_[0];
 }
 
-=item is_ctypes_compat($obj)
+=item is_ctypes_compat(obj)
 
-Returns 1 if object is Ctypes compatible - that is, it has a
-_as_param_, _update_ and _typecode_ methods, and the value returned
-by _typecode_ is valid. Returns undef otherwise.
+Returns 1 if C<obj> is Ctypes compatible - that is, it has a
+C<_as_param_>, C<_update_> and C<_typecode_> methods, and the value returned
+by C<_typecode_> is valid. Returns C<undef> otherwise.
 
 =cut
 
@@ -994,16 +995,16 @@ sub is_ctypes_compat (\$) {
 =item cast(obj, type)
 
 This function is similar to the cast operator in C. It returns a new
-instance of type which points to the same memory block as obj. type
+instance of type which points to the same memory block as C<obj>. C<type>
 must be a pointer type, and obj must be an object that can be
 interpreted as a pointer.
 
 =item create_string_buffer(init_or_size[, size])
 
 This function creates a mutable character buffer. The returned object
-is a ctypes array of c_char.
+is a Ctypes array of C<c_char>.
 
-init_or_size must be an integer which specifies the size of the array,
+C<init_or_size> must be an integer which specifies the size of the array,
 or a string which will be used to initialize the array items.
 
 If a string is specified as first argument, the buffer is made one
@@ -1013,14 +1014,14 @@ second argument which allows to specify the size of the array if the
 length of the string should not be used.
 
 If the first parameter is a unicode string, it is converted into an
-8-bit string according to ctypes conversion rules.
+8-bit string according to Ctypes conversion rules.
 
 =item create_unicode_buffer(init_or_size[, size])
 
 This function creates a mutable unicode character buffer. The returned
-object is a ctypes array of C<c_wchar>.
+object is a Ctypes array of C<c_wchar>.
 
-init_or_size must be an integer which specifies the size of the array,
+C<init_or_size> must be an integer which specifies the size of the array,
 or a unicode string which will be used to initialize the array items.
 
 If a unicode string is specified as first argument, the buffer is made
@@ -1030,19 +1031,19 @@ as second argument which allows to specify the size of the array if
 the length of the string should not be used.
 
 If the first parameter is a 8-bit string, it is converted into an
-unicode string according to ctypes conversion rules.
+unicode string according to Ctypes conversion rules.
 
 =item DllCanUnloadNow()
 
 Windows only: This function is a hook which allows to implement
-inprocess COM servers with ctypes. It is called from the
-DllCanUnloadNow function that the Ctypes XS extension dll exports.
+in-process COM servers with Ctypes. It is called from the
+C<DllCanUnloadNow> function that the Ctypes XS extension dll exports.
 
 =item DllGetClassObject()
 
 Windows only: This function is a hook which allows to implement
-inprocess COM servers with ctypes. It is called from the
-DllGetClassObject function that the Ctypes XS extension dll exports.
+in-process COM servers with ctypes. It is called from the
+C<DllGetClassObject> function that the Ctypes XS extension dll exports.
 
 =item FormatError([code])
 
@@ -1056,74 +1057,74 @@ Windows only: Returns the last error code set by Windows in the calling thread.
 
 =item memmove(dst, src, count)
 
-Same as the standard C memmove library function: copies count bytes
-from src to dst. dst and src must be integers or ctypes instances that
-can be converted to pointers.
+Same as the standard C memmove library function: copies count bytes from C<src>
+to C<dst>. C<dst> and C<src> must be integers or Ctypes instances that can be
+converted to pointers.
 
 =item memset(dst, c, count)
 
 Same as the standard C memset library function: fills the memory block
-at address dst with count bytes of value c. dst must be an integer
-specifying an address, or a ctypes instance.
+at address C<dst> with C<count> bytes of value C<c>. C<dst> must be an integer
+specifying an address, or a Ctypes instance.
 
 =item POINTER(type)
 
-This factory function creates and returns a new ctypes pointer
+This factory function creates and returns a new Ctypes pointer
 type. Pointer types are cached an reused internally, so calling this
-function repeatedly is cheap. type must be a ctypes type.
+function repeatedly is cheap. C<type> must be a Ctypes type.
 
 =item pointer(obj)
 
-This function creates a new pointer instance, pointing to obj. The
-returned object is of the type POINTER(type(obj)).
+This function creates a new pointer instance, pointing to C<obj>. The
+returned object is of the type C<POINTER(type(obj))>.
 
 Note: If you just want to pass a pointer to an object to a foreign
-function call, you should use byref(obj) which is much faster.
+function call, you should use C<byref(obj)> which is much faster.
 
 =item resize(obj, size)
 
-This function resizes the internal memory buffer of obj, which must be
-an instance of a ctypes type. It is not possible to make the buffer
+This function resizes the internal memory buffer of C<obj>, which must be
+an instance of a Ctypes type. It is not possible to make the buffer
 smaller than the native size of the objects type, as given by
-sizeof(type(obj)), but it is possible to enlarge the buffer.
+C<sizeof(type(obj))>, but it is possible to enlarge the buffer.
 
 =item set_conversion_mode(encoding, errors)
 
-This function sets the rules that ctypes objects use when converting
+This function sets the rules that Ctypes objects use when converting
 between 8-bit strings and unicode strings. encoding must be a string
 specifying an encoding, like 'utf-8' or 'mbcs', errors must be a
 string specifying the error handling on encoding/decoding
 errors. Examples of possible values are "strict", "replace", or
 "ignore".
 
-set_conversion_mode returns a 2-tuple containing the previous
-conversion rules. On windows, the initial conversion rules are
+C<set_conversion_mode> returns a 2-tuple containing the previous
+conversion rules. On Windows, the initial conversion rules are
 ('mbcs', 'ignore'), on other systems ('ascii', 'strict').
 
 =item sizeof(obj_or_type)
 
-Returns the size in bytes of a ctypes type or instance memory
-buffer. Does the same as the C sizeof() function.
+Returns the size in bytes of a Ctypes type or instance memory
+buffer. Does the same as the C C<sizeof()> function.
 
 =item string_at(address[, size])
 
 This function returns the string starting at memory address
-address. If size is specified, it is used as size, otherwise the
+C<address>. If C<size> is specified, it is used as size, otherwise the
 string is assumed to be zero-terminated.
 
 =item WinError( { code=>undef, descr=>undef } )
 
 Windows only: this function is probably the worst-named thing in
-Ctypes. It creates an instance of WindowsError.
+Ctypes. It creates an instance of L<WindowsError>.
 
-If B<code> is not specified, GetLastError is called to determine the
+If B<code> is not specified, L<GetLastError> is called to determine the
 error code. If B<descr> is not spcified, FormatError is called to get
 a textual description of the error.
 
-=item wstring_at(address)
+=item wstring_at(address [, size])
 
 This function returns the wide character string starting at memory
-address address as unicode string. If size is specified, it is used as
+address C<address> as unicode string. If C<size> is specified, it is used as
 the number of characters of the string, otherwise the string is
 assumed to be zero-terminated.
 
@@ -1180,7 +1181,7 @@ L<http://search.cpan.org/dist/Ctypes/>
 =head1 SEE ALSO
 
 There are 4 other Perl ffi libraries:
-L<Win32::API>, L<C::DynaLib>, L<FFI> and L<P5NCI>.
+  L<Win32::API>, L<C::DynaLib>, L<FFI> and L<P5NCI>.
 
 You'll need the headers and/or description of the foreign library.
 

lib/Ctypes/Function.pm
@@ -435,9 +435,11 @@ be one of three things:
 =item A linker argument style string, e.g. '-lc' for libc.
 
 For Win32, mingw and cygwin special rules are used:
-"c" resolves on Win32 to msvcrt<ver>.dll.
--llib will probably find an import lib ending with F<.a> or F<.dll.a>),
+"c" resolves on Win32 to F<msvcrtxx.dll>.
+
+F<-llib> will probably find an import lib ending with F<.a> or F<.dll.a>),
 so C<dllimport> is called to find the DLL behind.
+
 DLL are usually versioned, import libs not,
 so specifying the unversioned library name will find the most recent DLL.
 

lib/Ctypes/Type.pm
@@ -18,7 +18,7 @@ use Ctypes::Type::Struct;
 use Scalar::Util qw|looks_like_number|;
 use B qw|svref_2object|;
 use Encode;
-my $Debug;
+my $Debug = 1;
 use utf8;
 
 =head1 NAME
@@ -91,7 +91,7 @@ our $_pytypes =
            my $name = $Ctypes::Type::_types->{$tc}->{name};
            # my $MIN = $Ctypes::Type::_types->{$tc}->{MIN};
            # my $MAX = $Ctypes::Type::_types->{$tc}->{MAX};
-           print "In hook_in\n" if $Debug;
+           print "In hook_in c_byte($arg)\n" if $Debug;
            return ( "$name: cannot take references", undef )
              if ref($arg);
            if( Ctypes::Type::is_a_number($arg) ) {
@@ -382,7 +382,7 @@ our $_pytypes =
            my $name = $Ctypes::Type::_types->{$tc}->{name};
            my $MIN  = Ctypes::constant('PERL_INT_MIN');
            my $MAX  = Ctypes::constant('PERL_INT_MAX');
-           print "In hook_in\n" if $Debug;
+           print "In hook_in c_int($arg)\n" if $Debug;
            return ( "$name: cannot take references", undef )
              if ref($arg);
            if( Ctypes::Type::is_a_number($arg) ) {
@@ -649,7 +649,7 @@ sub _new {
   my $class = ref($_[0]) || $_[0];
   my $init = $_[1] if @_ > 1;
   my $self = {
-    _data       =>  "\0",             # raw (binary) memory block
+    _data       =>  "\0",          # raw (binary) memory block
     _needsfree  =>  0,             # does object own its data? (not used yet, 0.002)
     _owner      =>  undef,         # ref to object that owns this one
     _size       =>  0,             # size of memory block in bytes
@@ -818,7 +818,7 @@ sub typecode { return $_[0]->{_typecode} }
 
 This class method can cease all toleration of incorrect input
 for all Type objects. Sets/returns 1 or 0. See the
-L<strict_input/Ctypes::Type::Simple/allow_overflow> object method
+L<strict_input/Ctypes::Type::Simple/strict_input> object method
 for how to do this to individual objects
 
 =back
@@ -832,7 +832,7 @@ for how to do this to individual objects
     # with an object instead of a 1 or 0 and user would not be notified
     my $arg = shift;
     if( @_ or ( defined($arg) and $arg != 1 and $arg != 0 ) ) {
-      croak("Usage: allow_overflow_all(x) (1 or 0)");
+      croak("Usage: strict_input_all(x) (1 or 0)");
     }
     $strict_input_all = $arg if defined $arg;
     return $strict_input_all;

lib/Ctypes/Type/Field.pm
@@ -47,8 +47,7 @@ sub new {
                 _rawcontents => undef,
               };
   $self->{_rawcontents} = tie $self->{_contents},
-                          'Ctypes::Type::Field::contents',
-                          $self;
+    'Ctypes::Type::Field::contents', $self;
   $self->{_contents} = $val;
   return bless $self => $class;
 }
@@ -62,7 +61,7 @@ my %access = (
   size              => ['_size'],
   'index'           => ['_index'],
   owner             => ['_owner'],
-             );
+);
 for my $func (keys(%access)) {
   no strict 'refs';
   my $key = $access{$func}[0];
@@ -214,7 +213,8 @@ sub STORE {
 
 sub FETCH {
   my $self = shift;
-  print "In ", $self->{_obj}{_obj}->name, "'s ", $self->{_obj}{_key}, " field FETCH,\n\tcalled from ", (caller(1))[0..3], "\n" if $Debug;
+  print "In ", $self->{_obj}{_obj}->name, "'s ", $self->{_obj}{_key},
+    " field FETCH,\n\tcalled from ", (caller(1))[0..3], "\n" if $Debug;
   if( defined $self->{VALUE}
       and $self->{VALUE}->isa('Ctypes::Type::Simple') ) {
     return ${$self->{VALUE}};

lib/Ctypes/Type/Union.pm
@@ -7,7 +7,7 @@ use base qw|Ctypes::Type::Struct|;
 use Carp;
 use Data::Dumper;
 
-my $Debug = 0;
+my $Debug;
 
 ###########################################
 # TYPE::UNION : PUBLIC FUNCTIONS & VALUES #
@@ -36,11 +36,11 @@ sub new {
   $newname = 'Union' if $newname eq 'Union_Union';
   $self->_set_name($newname);
 
-  # ??? Will this be ok or need to explicitly undef all?
+  # XXX Will this be ok or need to explicitly undef all?
   for( keys %{$self->fields} ) {
     if( defined $self->fields->{$_} ) {
-    $self->fields->{$_}->_datasafe(0);
-    $self->fields->{$_}->_set_owner($self);
+      $self->fields->{$_}->_datasafe(0);
+      $self->fields->{$_}->_set_owner($self);
     }
   }
 
@@ -76,8 +76,8 @@ sub data {
     print "    returning ", unpack('b*',$self->{_data}), "\n" if $Debug;
     return \$self->{_data};
   }
-# TODO This is where a check for an endianness property would come in.
-#  if( $self->{_endianness} ne 'b' ) {
+  # TODO This is where a check for an endianness property would come in.
+  #  if( $self->{_endianness} ne 'b' ) {
     for(@{$self->{_fields}->{_rawarray}}) {
       push @data, $_->{_data};
     }

commit a73bffc6334d40bdaf929361824be34fd7c2a417

Fixed Pointer regression from commit 3edca28e35d6a
See comment near change.

Note there is still a Heisenbug in later Pointer test on this
commit (test 23).

	modified:   lib/Ctypes/Type/Simple.pm

lib/Ctypes/Type/Simple.pm
@@ -278,7 +278,9 @@ sub _update_ {
       $self->{_data} = pack($self->packcode, $self->{_value});
     }
   } else {
-    $self->{_data} = pack($self->packcode, $arg );
+# Don't need to pack() anything; _data should only ever be
+# be pased raw bytes.
+    $self->{_data} = $arg;
     if( $self->owner ) {
       $self->owner->_update_($self->{_data}, $self->{_index});
     }